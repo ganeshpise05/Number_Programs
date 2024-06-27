f10c0622c91138930828cf774808262a4f1afd9a
Ganesshsp05 <piseganesh2205@gmail.com>
2024-06-27 13:49:21 +0530

    Number Program: Even Odd

---
+++ Number_Programs/src/Number_Programs/Even_Odd.java
@@ -0,0 +1,15 @@
+package Number_Programs;
+
+public class Even_Odd {
+	
+	public static void main(String[] args) {
+		
+		int num = 10;
+		if (num%2==0) {
+			System.out.println("The given number "+num+" is even number.");
+		} else {
+			System.out.println("The given number "+num+" is odd number.");
+		}
+	}
+
+}
